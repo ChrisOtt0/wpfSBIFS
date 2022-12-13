91b79564dce6a08915577678770ad2a8172030ef
ChrisOtt0 <sophusott@gmail.com>
2022-12-13 15:33:21 +0100

    Fixed Bindable not being abstract.

--- Tools/Bindable.cs
+++ Tools/Bindable.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace wpfSBIFS.Tools
 {
-    public class Bindable : INotifyPropertyChanged
+    public abstract class Bindable : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
